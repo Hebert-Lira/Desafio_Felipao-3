2766f5cda6bf379bd17f6f977f2c20d2c2e08e32
Hebert-Lira <hebertlira558@gmail.com>
2026-04-08 22:09:32 -0300

    atualização

--- index1.js
+++ index1.js
@@ -1,1 +1,18 @@
-jesus
+class heroi {
+    constructor(type, name, magic) { 
+        this.type = type
+        this.name = name
+        this.magic = magic
+    }
+    ataque() {
+    console.log(`O ${this.type} ${this.name} atacou usando ${this.magic}`)
+}
+}
+     let mago = new heroi("mago","Gandalf",  "magia")
+     let guerreiro = new heroi("guerreiro","Aragorn", "espada")
+     let monge = new heroi("monge","Frodo", "artes marciais")
+     let ninja = new heroi("ninja","Legolas", "shuriken")
+
+        mago.ataque()
+
+    
